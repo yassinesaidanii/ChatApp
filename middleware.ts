766bbab812cce79ce5d142eabca1f417016374c0
yassinesaidanii <saidanidhia61@gmail.com>
2024-10-15 13:03:41 +0100

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
-// This example protects all routes including api/trpc routes
-// Please edit this to allow other routes to be public as needed.
-// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
 export default authMiddleware({
-  publicRoutes: ["/api/uploadthing"],
+  publicRoutes: ["/api/uploadthing", "/api/socket/io"], // Make this route public
 });
 
 export const config = {
