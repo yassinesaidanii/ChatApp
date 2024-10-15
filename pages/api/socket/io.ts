39d024fcff2bec57e9e51ffe0b359ff06ffc4fbf
yassinesaidanii <saidanidhia61@gmail.com>
2024-10-15 17:39:27 +0100

    Update io.ts

--- pages/api/socket/io.ts
+++ pages/api/socket/io.ts
@@ -1,8 +1,6 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import { Server as NetServer } from "http";
+import { Server as NextServer } from "http";
 import { NextApiRequest } from "next";
-import { Server as ServerIo } from "socket.io";
-
+import { Server as SocketIOServer } from "socket.io";
 import { NextApiResponseServerIo } from "@/types";
 
 export const config = {
@@ -13,15 +11,19 @@ export const config = {
 
 const ioHandler = (req: NextApiRequest, res: NextApiResponseServerIo) => {
   if (!res.socket.server.io) {
+    console.log("*First use, starting socket.io");
     const path = "/api/socket/io";
-    const httpServer: NetServer = res.socket.server as any;
-    const io = new ServerIo(httpServer, {
-      path: path,
-      addTrailingSlash: false,
-    });
-    res.socket.server.io = io;
-  }
 
+    const httpServer: NextServer = res.socket.server as any;
+    const io = new SocketIOServer(httpServer, {
+      path: path,
+      // @ts-ignore
+    });
+
+    res.socket.server.io = io;
+  } else {
+    console.log("socket.io already running");
+  }
   res.end();
 };
 
