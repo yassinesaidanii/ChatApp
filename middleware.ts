61195455b09f479c626519a185eeef84e155a678
yassinesaidanii <saidanidhia61@gmail.com>
2024-10-15 13:11:17 +0100

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
   ignoredRoutes: ["/api/socket/io"], // Ignore this route from Clerk authentication
-  publicRoutes: ["/api/uploadthing"],
+  publicRoutes: ["/api/uploadthing", "/(.*)"],
 });
 
 export const config = {
