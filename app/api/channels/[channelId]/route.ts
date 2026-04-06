26f06353a20a63044d4761be9b87fb90a073fdce
yassinesaidanii <168484168+yassinesaidanii@users.noreply.github.com>
2026-04-06 18:08:57 +0100

    Force dynamic rendering for channel route

--- app/api/channels/[channelId]/route.ts
+++ app/api/channels/[channelId]/route.ts
@@ -3,6 +3,8 @@ import { db } from "@/lib/db";
 import { MemberRole } from "@prisma/client";
 import { NextResponse } from "next/server";
 
+export const dynamic = "force-dynamic";
+
 export async function DELETE(
   req: Request,
   { params }: { params: { channelId: string } }
